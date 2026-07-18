ef8a2d8a84a468db6414c2badc95762181ba6686
lucasronan33 <164685834+lucasronan33@users.noreply.github.com>
2026-07-18 00:12:06 -0300

    feat(Header): refatora componente Header e adiciona estilos para título e subtítulo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,69 +1,91 @@
-import React, { useEffect } from 'react'
-import './style.css'
-import { Plus } from 'lucide-react'
-import NewBudget from '../NewBudget'
-import { Subtitle, Title } from './styles'
-import { Button } from '../Button'
-import Sidebar from '../Sidebar'
-import { useBudget } from '../../context/Budget'
-import { useDispatch, useSelector } from 'react-redux'
-import { budgetReset } from '../../store/modules/budget/actions'
-import { useNavigate } from 'react-router-dom'
+import { Plus } from "lucide-react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useBudget } from "../../context/Budget";
+import { budgetReset } from "../../store/modules/budget/actions";
+import { Button } from "../Button";
+import NewBudget from "../NewBudget";
+import Sidebar from "../Sidebar";
+import "./style.css";
 
 export default function Header() {
-    const { budgetOpen, setBudgetOpen, setBudget, initialState } = useBudget()
-    const { user } = useSelector(state => state.auth)
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+    const { budgetOpen, setBudgetOpen, setBudget, initialState } = useBudget();
+    const { user } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
-
         if (!budgetOpen) {
-            document.body.removeAttribute('style')
-            dispatch(budgetReset())
-            setBudget(initialState)
-            return
+            document.body.removeAttribute("style");
+            dispatch(budgetReset());
+            setBudget(initialState);
+            return;
         }
         if (budgetOpen) {
-            document.body.style.overflow = 'hidden'
-            return
+            document.body.style.overflow = "hidden";
+            return;
         }
-    }, [
-        budgetOpen,
-        initialState,
-        dispatch,
-        setBudget
-    ])
+    }, [budgetOpen, initialState, dispatch, setBudget]);
 
     return (
-        <div className='header'>
-            {budgetOpen && (
-                <NewBudget />
-            )}
-            <div className='container'>
-                <div className='container-logo-title'>
+        <div
+            className="
+        w-full
+        min-h-[10vh]
+        flex
+        items-center
+        justify-around
+        py-[2.5vh]
+        "
+        >
+            {budgetOpen && <NewBudget />}
+            <div
+                className="
+            w-[90%]
+            flex
+            justify-around
+            items-center
+            flex-auto
+            flex-wrap
+            gap-[3vh]
+            "
+            >
+                <div
+                    className="
+                flex
+                items-center
+                justify-between
+                gap-[3vh]
+                "
+                >
                     <Sidebar />
+                    <div className="logo" onClick={() => navigate("/")} />
                     <div
-                        className='logo'
-                        onClick={() => navigate('/')}
-                    />
-                    <div className='container-title'>
-                        <Subtitle className='subtitle-header'>
-                            Bem-vindo ao ORCA
-                        </Subtitle>
-                        <Title>
-                            {user.name}
-                        </Title>
+                        className="
+                    h-full
+                    flex
+                    flex-1
+                    flex-col
+                    justify-around
+                    gap-y-[0.7vh]
+                    "
+                    >
+                        <div className="subtitle">Bem-vindo ao ORCA</div>
+                        <div className="title">{user.name}</div>
                     </div>
                 </div>
-                <Button.Root className='button-header' onClick={() => {
-                    setBudgetOpen(true)
-                }}>
-                    <Plus />
-                    Novo Orçamento
-                </Button.Root>
-
+                <div>
+                    <Button.Primary
+                        onClick={() => {
+                            setBudgetOpen(true);
+                        }}
+                    >
+                        <Plus />
+                        Novo Orçamento
+                    </Button.Primary>
+                </div>
             </div>
         </div>
-    )
+    );
 }
