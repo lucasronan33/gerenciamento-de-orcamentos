f34e6751e383336c6f3971b3ec88d994f27db0cd
lucasronan33 <164685834+lucasronan33@users.noreply.github.com>
2026-08-20 12:46:57 -0300

    refactor(Header): ajusta a formatação e o estilo do componente Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -66,13 +66,13 @@ export default function Header() {
             <div className="logo" onClick={() => navigate("/")} />
             <div
               className="
-            h-full
-            flex
-            flex-1
-            flex-col
-            justify-around
-            gap-y-[0.7vh]
-            "
+              h-full
+              flex
+              flex-1
+              flex-col
+              justify-around
+              gap-y-[0.7vh]
+              "
             >
               <div className="subtitle">Bem-vindo ao ORCA</div>
               <div className="title">{user.name}</div>
@@ -87,7 +87,11 @@ export default function Header() {
           justify-end
         "
         >
-          <div>
+          <div
+            className="
+            max-sm:w-full
+          "
+          >
             <Button.Primary
               onClick={() => {
                 setBudgetOpen(true);
